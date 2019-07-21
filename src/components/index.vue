fbad565faab72f8adfabaee1df736b5df60591da
VPLand <vpland@outlook.com>
2019-07-21 16:32:17 +0800

    update

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="text-center mt-20 text-20 text-weight-bold">VUE2.0模板</div>
+    <div>test</div>
     <!-- <img class="text-center" src="../../static/logo.png" alt=""> -->
   </div>
 </template>
